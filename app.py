bd0a1e58cbe20b0f90940824f65770262beab6be
kaustuv Kunal <kaustuv.kunal@greatlearning.in>
2025-01-02 23:06:30 +0530

    minor

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.subheader(body="Upload your Resume ( in PDF form )")
 pdf_doc = st.file_uploader("Please upload the Resume in pdf format here : ",type =['pdf'])
 
 st.subheader("Enter the Job Description 📋", divider = True)
-job_desc= st.text_area(label = " Paste the Job Description Below ", max_chars=10000)
+job_desc= st.text_area(label = " Copy-Paste the Job Description Below ", max_chars=10000)
 
 submit = st.button(label="Invoke CV Assistent")
 if submit:
