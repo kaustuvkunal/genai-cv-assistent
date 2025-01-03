2cb3b1b115bc2fa5350ffa5b3f2eaebb6c37bd37
K Kunal <kaustuv.kunal@gmail.com>
2025-01-03 16:26:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-# Set up the Local Environment
-# from dotenv import load_dotenv
-# load_dotenv() # setup local environment
 import streamlit as st
-#import google.generativeai as genai
-#from pdf import read_pdf
 from cv_evaluator import profile
 
 import warnings
@@ -12,7 +7,6 @@ warnings.filterwarnings("ignore")
 
 
 # Lets setup streamlit front end and read the pdf document
-#st.set_page_config("Career Compass")
 
 st.header(" CV Assistant for Recruiters & Applicants ", divider = "blue")
 st.subheader(" ")
@@ -28,10 +22,3 @@ job_desc= st.text_area(label = " Copy-Paste the Job Description Below ", max_cha
 submit = st.button(label="Invoke CV Assistent")
 if submit:
     st.markdown(profile(pdf_doc=pdf_doc, job_desc=job_desc))
-    
-
-# st.subheader("Disclaimer: ", divider = True)
-# notes = f'''
-# 1. To Be Added
-# '''
-# st.write(notes)
